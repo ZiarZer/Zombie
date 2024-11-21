74bcf515456d65ed390d527a2b6f4a0795eb5b45
ZiarZer <dev@ziarzer.fr>
2024-11-21 23:47:01 +0100

    (test) update and cleanup testsuite

--- tests/check.sh
+++ tests/check.sh
@@ -6,6 +6,15 @@ blue='\e[34;1m'
 normal='\e[0m'
 bold='\e[1m'
 bin='zombie'
+FAILURE_text="$bold[ ${red}FAILURE$normal$bold ]$normal"
+SUCCESS_text="$bold[ ${green}SUCCESS$normal$bold ]$normal"
+
+print_FAILURE() {
+    printf "$FAILURE_text "
+}
+print_SUCCESS() {
+    printf "$SUCCESS_text "
+}
 
 echo >tests/stderr
 echo >tests/stdout
@@ -16,89 +25,74 @@ printf "$blue\t===============================$normal\n\n"
 total=0
 success=0
 
-compare_results() {
-    if [ "$?" -ne "$1" ]; then
-        text="$bold[ ${red}FAILURE$normal$bold ]$normal"
-    else
-        diff tests/stdout tests/expected/"$file"
+compare_output_files() {
+    received=$(cat "$1")
+    if [ -f "$2" ]; then
+        diff "$1" "$2"
         if [ "$?" -ne 0 ]; then
-            text="$bold[ ${red}FAILURE$normal$bold ]$normal"
-        else
-            text="$bold[ ${green}SUCCESS$normal$bold ]$normal"
-            success=$((success + 1))
+            passed_test=1
         fi
+    elif [ -n "$received" ]; then
+        passed_test=1
     fi
-    printf "$text $file.bf\n"
+}
+
+run_test() {
+    expected_return="$1"; shift
+    file="$1"; shift
+    input="$1"; shift
+    passed_test=0
+
+    printf "$input" | ./"$bin" "$@" tests/"$file".bf 2> tests/stderr 1> tests/stdout
+
+    if [ "$?" -ne "$expected_return" ]; then
+        passed_test=1
+    fi
+
+    outputfile="$file"
+    params_string=''
+    if [ "$#" -gt 0 ]; then
+        params_string=$(echo "$@" | sed 's/ //g')
+        outputfile="${outputfile}-$params_string"
+    fi
+    compare_output_files tests/stdout tests/expected/"$outputfile".output
+    compare_output_files tests/stderr tests/expected/"$outputfile".error
+
+    if [ "$passed_test" -eq 0 ]; then
+        print_SUCCESS
+        success=$((success + 1))
+    else
+        print_FAILURE
+    fi
+    printf "$file.bf"
+    if [ -n "$params_string" ]; then
+        printf " ($*)"
+    fi
+    printf "\n"
     total=$((total + 1))
 }
 
-simple_test() {
-    file="$2"
-    ./"$bin" tests/"$file".bf 2> tests/stderr 1> tests/stdout
+run_test 1 file_not_found
+run_test 2 missing_left
+run_test 2 missing_right
+run_test 3 endless-add
+run_test 3 incorrect_value_upper
+run_test 3 incorrect_value_lower
+run_test 3 pointer_out_of_bounds
+run_test 3 multiple_errors
+run_test 3 multiple_errors       ''             -a 4
+run_test 3 repeat-forever       'out of bounds'
 
-    compare_results "$1"
-}
+run_test 0 hello_world
+run_test 0 trailing-comments
+run_test 0 comments
+run_test 0 nine
+run_test 0 simple-loop
 
-input_test() {
-    file="$2"
-    input="$3"
-    printf "$input" | ./"$bin" tests/"$file".bf 2> tests/stderr 1> tests/stdout
+run_test 0 add                   '!#'
+run_test 0 mul                   "\n\n"
 
-    compare_results "$1"
-}
-
-error_test() {
-    file="$2"
-    if [ "$#" -eq 2 ]; then
-        ./"$bin" tests/"$file".bf 2> tests/stderr 1> tests/stdout
-    else
-        error_test "$1" "$2"
-        ./"$bin" tests/"$file".bf -a "$3" 2> tests/stderr 1> tests/stdout
-    fi
-
-    if [ "$?" -ne "$1" ]; then
-        text="$bold[ ${red}FAILURE$normal$bold ]$normal"
-    else
-        expected_output_file=tests/expected/"$file"
-        if [ "$#" -gt 2 ]; then
-            expected_output_file="${expected_output_file}_with_size"
-        fi
-        diff tests/stderr "$expected_output_file"
-        if [ "$?" -ne 0 ]; then
-            text="$bold[ ${red}FAILURE$normal$bold ]$normal"
-        else
-            text="$bold[ ${green}SUCCESS$normal$bold ]$normal"
-            success=$((success + 1))
-        fi
-    fi
-
-    if [ "$#" -gt 2 ]; then
-        printf "$text $file.bf (with array size equal to $3)\n"
-    else
-        printf "$text $file.bf\n"
-    fi
-
-    total=$((total + 1))
-}
-
-error_test 1 file_not_found
-error_test 2 missing_left
-error_test 2 missing_right
-error_test 3 incorrect_value_upper
-error_test 3 incorrect_value_lower
-error_test 3 pointer_out_of_bounds
-error_test 3 multiple_errors 4
-
-simple_test 0 hello_world
-simple_test 0 comments
-simple_test 0 nine
-
-input_test 0 add '!#'
-input_test 0 mul "\n\n"
-
-simple_test 0 ZiarZer
-
-echo
+run_test 0 ZiarZer
 
 percent=$((success * 100 / total))
 bars=$((percent * 2 / 10))
